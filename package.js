c7c7e5078138826fd18fd7097b7d5d330b88bfc3
Christian Storch <ccstorch@gmail.com>
2016-07-05 14:27:41 +0100

    Version 0.0.3

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'cinn:collection-helpers',
-  version: '0.0.2',
+  version: '0.0.3',
   summary: 'Helpers for meteor collections',
   git: 'https://github.com/cinn-labs/collection-helpers',
   documentation: 'README.md'
@@ -8,7 +8,7 @@ Package.describe({
 
 Package.onUse(function(api) {
   const both = ['client', 'server'];
-  api.versionsFrom('1.3.2.4');
+  api.versionsFrom('1.3.4');
   api.export('CollectionHelpers');
   api.use('ecmascript');
   api.addFiles('collection-helpers.js', both);
